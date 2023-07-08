a17a0b812698220a00c1cb73853e07b68001b504
viviennemi <136140265+viviennemi@users.noreply.github.com>
2023-07-08 06:44:36 +0000

    modify

--- game.py
+++ game.py
@@ -6,3 +6,14 @@ min=1
 max=10
 target = random.randrange(min,max)
 print(target)
+
+while true:
+    keyin = int(input(f'猜數字範圍{min}~{max}'))
+    count+=1
+    if(keyin == target):
+        print(f'猜對了,答案是{target}')
+        print(f'你共猜了{count}次')
+        break
+    else:
+        print(f'猜錯了,\n你已經猜了{count}次')
+print('Game Over')
