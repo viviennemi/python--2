323352b120a1374f912ba6800af48ffb96e6db36
viviennemi <136140265+viviennemi@users.noreply.github.com>
2023-07-08 07:15:25 +0000

    modify

---
+++ tgamr.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python 3.10.8
+
+import random
+
+min=1
+max=10
+count = 0  #記得要加上變數不然無法在執行迴圈時，程式找不到變數無法執行
+target = random.randrange(min,max)
+print('**猜數字**')
+
+while True:
+    keyin = int(input(f'猜數字範圍{min}~{max}:  '))
+    count += 1
+    if(keyin == target):
+        print(f'猜對了答案是{target}\n你已經猜了{count}次')
+        break
+    elif keyin <target:
+        print('再大一點')
+        min = keyin +1
+    elif keyin >target:
+        print('再小一點')
+        max = keyin-1
+    print(f'你已經猜了{count}次')
+print('Game Over')
