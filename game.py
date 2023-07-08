80348d090f8a3694db390e602c6cf68f55211baa
viviennemi <136140265+viviennemi@users.noreply.github.com>
2023-07-08 06:49:10 +0000

    modify

--- game.py
+++ game.py
@@ -4,12 +4,13 @@ import random
 
 min=1
 max=10
+count = 0
 target = random.randrange(min,max)
 #print(target)
 
 while True:
     keyin = int(input(f'猜數字範圍{min}~{max}'))
-    count+=1
+    count += 1
     if(keyin == target):
         print(f'猜對了,答案是{target}')
         print(f'你共猜了{count}次')
