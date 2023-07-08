c4f809f06253783a7f4ab88d3b3b98dc023a48f7
viviennemi <136140265+viviennemi@users.noreply.github.com>
2023-07-08 06:53:56 +0000

    modify

--- game.py
+++ game.py
@@ -12,9 +12,8 @@ while True:
     keyin = int(input(f'猜數字範圍{min}~{max}:  '))
     count += 1
     if(keyin == target):
-        print(f'猜對了,答案是{target}')
-        print(f'你共猜了{count}次')
+        print(f'猜對了,答案是{target}\n你共猜了{count}次')
         break
     else:
-        print(f'猜錯了,\n你已經猜了{count}次')
+        print(f'猜錯了\n你已經猜了{count}次')
 print('Game Over')
