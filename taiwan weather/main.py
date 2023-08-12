75f503b936f659a19c99391fc729bddbbc1ceccc
viviennemi <136140265+viviennemi@users.noreply.github.com>
2023-08-12 08:22:56 +0000

    modify

--- taiwan weather/main.py
+++ taiwan weather/main.py
@@ -82,11 +82,16 @@ dataFrame['啟始時間'] = pd.to_datetime(dataFrame['啟始時間'])
 dataFrame['結束時間'] = pd.to_datetime(dataFrame['結束時間'])
 dataFrame['啟始時間'] = dataFrame['啟始時間'].dt.strftime('%Y-%m-%d日-%H點')
 dataFrame['結束時間'] = dataFrame['結束時間'].dt.strftime('%Y-%m-%d日-%H點')
+dataFrame['最高溫度'] = dataFrame['最高溫度'].astype(int)
+dataFrame['最低溫度'] = dataFrame['最低溫度'].astype(int)
+#更改外觀樣式
+style = dataFrame.style.highlight_max(subset=['最高溫度'],axis=0,props="color:white;background-color:red;")
+style = style.highlight_max(subset=['最低溫度'],axis=0,props="color:white;background-color:blue;")
 #顯示標題
 st.title("台灣各縣市氣候:")
 st.subheader("攝氏")
 #顯非DataFrame
-st.dataframe(dataFrame,width=800,height=900)
+st.dataframe(style,width=800,height=900)
 
 st.line_chart(dataFrame,x='城市',y=['最高溫度','最低溫度'])
 
